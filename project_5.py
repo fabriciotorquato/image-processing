bbe888bf8e08b89340402adabfe3ce0c20032ecb
Fabricio Torquato <f.torquato@gmail.com>
2019-05-09 20:25:58 -0300

    try again

--- project_5.py
+++ project_5.py
@@ -5,21 +5,53 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('data/sudoku/sudoku1.jpg', cv2.IMREAD_GRAYSCALE)
 
-tresholdMin = 100
-tresholdMax = 200
+tresholdMin = 30
+tresholdMax = 60
 
-kernelSize = (5,5)
+kernelSize = (3, 3)
 
 # filtro = cv2.GaussianBlur(img,kernelSize,0)
+_, thresh = cv2.threshold(img, 247, 247, cv2.THRESH_BINARY_INV)
+canny = cv2.erode(thresh, kernelSize, iterations=1)
 
-# edges = cv2.Canny(filtro,tresholdMin,tresholdMax)
+# edges = cv2.erode(edges, kernelSize, iterations=1)
 
-sobel = cv2.Sobel(img,cv2.CV_8U,1,1,ksize=5)
+sobel = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=3)
+sobel = cv2.erode(sobel, kernelSize, iterations=1)
 
-cv2.imshow('SobelX',  sobel)
+# sobel = sobel - canny
+# sobel = cv2.Sobel(sobel,cv2.CV_8U,0,1,ksize=3)
 
-cv2.imshow('Original', img)
-# cv2.imshow('Canny',edges)
+
+# get contours
+contours, _ = cv2.findContours(
+    sobel, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+areas = []
+for contour in contours:
+    area = cv2.contourArea(contour)
+    if area > 0 :
+        areas.append(area)
+
+areas = np.asarray(areas)
+min = np.mean(areas)
+print(np.mean(areas))
+
+for contour in contours:
+
+    area = cv2.contourArea(contour)
+   
+    if area >= 80 and area <= 300:
+        print(area)
+        rect = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.polylines(img, [box],  True,  (0, 123, 123),  3)
+
+
+cv2.imshow('A',  canny)
+cv2.imshow('B',  sobel)
+cv2.imshow('C',  img)
 
 cv2.waitKey()
 
